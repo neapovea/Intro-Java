be884118c86183ca95f7846c5bacdd5d552e566c
neapovea <neapovea@gmail.com>
2018-02-22 23:18:50 +0100

    27 - Packages

---
+++ 27 - Packages/src/App.java
@@ -0,0 +1,17 @@
+import ocean.Fish;
+import ocean.plants.Seaweed;
+import ocean.plants.Algae;
+ 
+public class App {
+ 
+     
+    public static void main(String[] args) {
+        Fish fish = new Fish();
+
+
+        Seaweed weed = new Seaweed();
+
+
+    }
+ 
+}
