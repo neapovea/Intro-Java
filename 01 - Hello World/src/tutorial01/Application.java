3d8a002cd058d7c8a8465fc2e8570accd231c69f
neapovea <neapovea@gmail.com>
2018-02-22 13:54:35 +0100

    01 - Hello World

---
+++ 01 - Hello World/src/tutorial01/Application.java
@@ -0,0 +1,26 @@
+package tutorial01;
+
+public class Application {
+
+	/**
+	 * 
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		
+		String cadena = null;
+		
+		cadena = "Hello World ";
+		
+				
+		System.out.println(cadena + 11);
+		
+		
+		
+		
+	}
+
+}
